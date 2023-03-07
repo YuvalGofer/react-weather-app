c1734a40e4019647e6444e05acadb5b46a3034ca
yuval gofer <yuvyael@gmail.com>
2023-03-07 15:26:34 +0200

    Introduction - World wide weather app - stage 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+import CityComponent from "./modules/CityComponent";
+import { useState } from "react";
+import axios from "axios";
+
+const API_KEY = "847c87dc86225c6b4a0ef00d5e125129";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+  box-shadow: 0 3px 6px #555;
+  padding: 20px 10px;
+  border-radius: 4px;
+  width: 380px;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+  margin: 10px;
+`;
 
 function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    updateWeather(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppLabel>Worldwide Weather App</AppLabel>
+      {weather ? (
+        <WeatherComponent weather={weather}/>
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
   );
 }
 
